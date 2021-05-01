fcd8368bc026e9968239e7400c55ad3a350d18a5
hscevik <hscevik@hotmail.com>
2021-05-01 17:34:19 +0300

    Kodlama_IO Odev2

---
+++ odev2/Category.java
@@ -0,0 +1,16 @@
+package odev2;
+
+public class Category {
+
+	int 	id;
+	String 	name;
+	String	comment;
+	
+	public Category(int id,String name,String comment) {
+		//Constructor
+		this.id			=	id;
+		this.name		=	name;
+		this.comment	=	comment;
+				
+	}
+}
---
+++ odev2/Lecrute.java
@@ -0,0 +1,18 @@
+package odev2;
+
+public class Lecrute {
+	int 	id;
+	int		courseId;
+	int 	contentId; 
+	String	name;
+	
+	public Lecrute(int id,int courseId,int contentId,String name) {
+		//Constructor
+		this.id			=	id;
+		this.courseId	=	courseId;
+		this.contentId	=	contentId;
+		this.name		=	name;
+	}
+
+	
+}
